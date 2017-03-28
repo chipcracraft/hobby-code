525bb9e30918b1f808d4a79608c1ec945cb6003f
Chip C <chipcracraft@gmail.com>
2017-03-28 12:45:33 -0500

    added dropdown

--- js/app.js
+++ js/app.js
@@ -1,19 +1,12 @@
 console.log('wired up!')
 console.log($)
-console.log(_)
-console.log(Backbone)
-
-$(".sidebar-btn").click( function(a){
-      $(".wrapper").toggleClass("sideDisplay");
-
-});
 
 
-$("mobile-nav").hover(
-    function () {
-      $(this).toggleClass('datWhiteClass');
-    },
-   function () {
-       $(this).toggleClass('datWhiteClass');
-    }
-  );
+
+
+// $("#menu-toggle").click( function (a){
+//         a.preventDefault();
+//         $("#app-container").toggleClass(".side-list");
+//
+//
+// });
